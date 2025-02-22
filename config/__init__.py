4541c44e31eb9bd2cbb559275b729508f89ee560
André Leite Carvalho <66704150+carvalhoandre@users.noreply.github.com>
2025-02-21 21:23:11 -0300

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,4 +1,4 @@
-from os import getenv
+import os
 from flask import Flask
 from flask_cors import CORS
 
@@ -8,8 +8,10 @@ def create_app():
 
     from resources.email_resource import email_bp
 
-    cors_url = getenv('CORS_URL_ENV', 'http://localhost:3000')
-    CORS(app, resources={r"/*": {"origins": cors_url}},
+    cors_urls = os.getenv("CORS_URLS", "").split(",")
+    cors_urls = [url.strip() for url in cors_urls if url]
+
+    CORS(app, resources={r"/*": {"origins": cors_urls}},
          methods=["GET", "POST", "OPTIONS"],
          allow_headers=["Content-Type", "Authorization", "X-Requested-With"],
          supports_credentials=True)
@@ -17,3 +19,4 @@ def create_app():
     app.register_blueprint(email_bp)
 
     return app
+
